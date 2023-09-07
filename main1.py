90727137ff781a9634dea0eb5ae0990d01396c30
LauGds <137927575+LauGds@users.noreply.github.com>
2023-09-07 11:14:35 -0700

    main1.py

--- main1.py
+++ main1.py
@@ -2,10 +2,9 @@ print("Welcome to the tip calculator.")
 
 bill = float(input("What was the total bill? $"))
 people = int(input("How many people to split the bill? "))
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 
-calculate = bill * (tip / 100)
-bill_per_person = (bill + calculate) / people
-total = round(bill_per_person, 2)
-total = "{:.2f}".format(bill_per_person)
-print(f"Each person should pay: ${total}")
+tip = bill * (tip_percentage / 100)
+total_per_person = (bill + tip) / people
+
+print(f"Each person should pay: ${total_per_person:.2f}")
